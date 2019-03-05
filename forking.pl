247b76ad9ed316f878e6606b64ae8286acc7ebf3
hakmonkey <hakmonkey@gmail.com>
2019-03-04 20:10:12 -0700

    Generalized the Forking Script
    Removed some of the declarations made and added comments so that this
    script could be used for various commands that need to be run on a
    server with multiple input files.

--- forking.pl
+++ forking.pl
@@ -3,14 +3,20 @@
 use strict;
 use Parallel::ForkManager;
 
+# Input the max number of child precess you would like to run at once
+my $max_cpu = "";
+# Input the file type you would like to use ("*.gtk")
+my $type = "";
+
 my %data;
-my @files = glob("*.pdf");
-my $pm = Parallel::ForkManager->new(3);
+my @files = glob("$type");
+my $pm = Parallel::ForkManager->new($max_cpu);
 
 DATA_LOOP:
 foreach my $file (@files) {
   $pm->start and next DATA_LOOP;
-    # system
+    # Write the command you would like to execute here!
+    system "";
   $pm->finish;
 }
 $pm->wait_all_children;
