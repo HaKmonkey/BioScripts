20c86857aaa9ab3e916257f1fd2f483828a5668d
hakmonkey <hakmonkey@gmail.com>
2019-03-06 00:10:29 -0700

    Parsing Cigar Strings
    Filtered which nodes were being parsed further by tag (2048 or 0x800).
    Was able to get the sequences to be edited correctly (it looks that
    way) by parsing the cigar string and modifying the `seq` accordingly.
    
    Will need to make sure that annotations from `.gff` file are checked
    and 'moved' while the sequences are edited so that their position
    changes correctly.
    
    Need to re-look into nodes (parts of the alignment [`.sam` file]) that
    were removed just to make sure we get proper wrapping of the
    annotations.
    
    Will also need to clean up some of the test strings and comments to
    ensure readability for later because who wants to come back a couple
    years from now and have no idea what's going on...

--- annotation_to_alignment.py
+++ annotation_to_alignment.py
@@ -6,8 +6,12 @@
 ## ONLY MODIFY GFF DATA
 
 import csv, sys, re
+from cigar import Cigar
 
-inFile = []
+# Both of the input files
+# Index 0 is the `.sam` file from bwa
+# Index 1 is the `.gff` file from prokka
+inFile = [sys.argv[1], sys.argv[2]]
 samData = []
 gffData = []
 header = 0
@@ -15,14 +19,12 @@ header = 0
 # Might not need some of these either. Will edit after I figure out how to
 # - propperly edit the gff information
 samNodes = []
+samFlags = []
 samStart = []
 samSeq = []
-samLength = []
+samCigar = []
 
-# Both of the input files
-# Index 0 is the `.sam` file from bwa
-# Index 1 is the `.gff` file from prokka
-[inFile.append(sys.argv[i]) for i in range(1,3)]
+supAlign = []
 
 # Storing the `.sam` file data into a list
 with open(inFile[0], 'r') as sam:
@@ -44,14 +46,12 @@ for i in range(len(gffData)):
     if gffData[i] == ['##FASTA']:
         fasta = i
         break
-
 del gffData[fasta:len(gffData)]
 
 # Finding and removing the header to the `.gff` file because we don't need it
 for i in range(len(gffData)):
     if re.search(r"\#", str(gffData[i])):
         header += 1
-
 del gffData[0:header]
 
 # Need position 1 [index 0] (Node name), position 4 [index 3] (left most base),
@@ -60,15 +60,108 @@ for i in range(len(samData)):
     temp = str(samData[i])
     temp = temp.split(" ")
     node = temp[0]
+    flag = temp[1]
+    flag = flag[1:len(flag)-2]
     start = temp[3]
     start = start[1:len(start)-2]
     seq = temp[9]
     seq = seq[1:len(seq)-2]
     node = node[2:len(node)-2]
+    cigar = temp[5]
+    cigar = cigar[1:len(cigar)-2]
     samNodes.append(node)
+    samFlags.append(flag)
     samStart.append(start)
     samSeq.append(seq)
-    samLength.append(len(seq))
+    samCigar.append(cigar)
+
+# Filter `.sam` data
+# `.sam` enteries that have the 2048 flag (0x800) are supplimentary alignments
+# - and are clipped when viewing the alignments
+# So these entries need to be removed so the annotations can be ligned up with
+# - the parts of the nodes that are present in the actual alignment
+for i in range(len(samNodes)):
+    if samFlags[i] == '2048':
+        supAlign.append(i)
+
+node = []
+start = []
+seq = []
+flag = []
+cigar = []
+
+for i in range(len(samNodes)):
+    if  (i in supAlign) == False:
+        node.append(samNodes[i])
+        start.append(samStart[i])
+        seq.append(samSeq[i])
+        flag.append(samFlags[i])
+        cigar.append(samCigar[i])
+
+# Now we need to adjust the sequences that remain to match their cigar string so
+# - that everything will match nicely with the alignment
+
+# Case1 ( M/X/=):
+# - start at the specified mapping position, set counter to 1
+# - Add 1 to both the counts of the bases from that position and the counter.
+# - Move to the next position.
+# - Repeat this process till counter is the same as the number associated with
+# - the operator.
+## SIMPLY COUNT THE BASES PRESENT
+
+# Case2 (N/D):
+# - Move the specified mapping position by the number associated with
+# - the operator.
+## ADD SPACE INTO READ
+
+# Case3 (I/S/H/P):
+# - Do nothing
+## P ADDS SPACE TO READ ? (DON'T KNOW YET, NO READS HAVE HAD PADDING!)
+## S/H CLIP FROM READ (SHORTEN IT)
+## I REMOVES BASES FROM READ (looks that way in tablet and artemis)
+for i in range(len(seq)):
+    smoke = re.findall(r'([\d]+)([A-Z])', cigar[i])
+    base_num = 1
+    temp_seq = list(seq[i])
+    print(node[i])
+    print("start len seq: " + str(len(temp_seq)))
+    print("start base num: " + str(base_num))
+    for j in range(len(smoke)):
+        count = 0
+        op = smoke[j]
+        if op[1] == 'M' or op[1] == 'X' or op[1] == '=':
+            # iterate through the sequence without modifying the base
+            while count != int(op[0]):
+                base_num += 1
+                count += 1
+            print("iter")
+            print("current base num: " + str(base_num))
+        elif op[1] == 'N' or op[1] == 'D':
+            # add an N to the read to inflate its length
+            while count != int(op[0]):
+                temp_seq.insert(base_num-1, 'N')
+                base_num += 1
+                count += 1
+            print("add")
+            print("current base num: " + str(base_num))
+        elif op[1] == 'S' or op[1] == 'H' or op[1] == 'I':
+            # Remove the bases from a range
+            while count != int(op[0]):
+                del temp_seq[base_num-1]
+                count +=1
+            print("remove")
+            print("current base num: " + str(base_num))
+        else:
+            # I don't yet know how padding affects the reads for this...
+            print("P")
+    print("end len seq: " + str(len(temp_seq)))
+    print("end base num: " + str(base_num))
+    print(" ")
+
+print(cigar[23])
+
+    #print(len(temp_seq))
+    #print(count)
 
 # Need position 4 [index 3] (feature start), position 5 [index 4] (feature end)
 for i in range(len(gffData)):
@@ -78,9 +171,9 @@ for i in range(len(gffData)):
     # - change temp[4]
     # - gffData[i] = temp
 
-print(samNodes[3])
-print(samStart[3])
-print(samLength[3])
-print(samNodes[4])
-print(samStart[4])
-print(samLength[5])
+#for i in range(len(node)):
+    #print(node[i])
+    #print(start[i])
+    #print(cigar[i])
+    #print(flag[i])
+    #print(" ")
