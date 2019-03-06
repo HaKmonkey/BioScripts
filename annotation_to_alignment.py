5a03de6d3098cde99eabe4f1c96ae31efa977096
hakmonkey <HaKmonkey@gmail.com>
2019-03-06 12:22:25 -0700

    Fixed Sequence Parsing
    Sequences are now reversed where necessary before they are processed
    by their cigar string.
    
    Created a test `for` loop that further inflates the sequence with a
    temp variable '$'. just realized that I need to replace the base at
    `base_num-1` with the temp variable instead of inflating. This way
    the sequence length is maintained instead of modified; annotations
    can be mapped to the 'new' sequence, and modified when the '$' are
    removed from the reads.
    
    when working `.gff` output is confirmed the number of lists will also
    be reduced.
      - I created some additional lists to parse the `.sam` data
          instead of modifying the original data
      - Once everything is working the original lists can be modified
          instead of new lists

--- annotation_to_alignment.py
+++ annotation_to_alignment.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
 
-# will be using positional information from sam file to inform positional
-# - changes in the gff data. This data will then be output as a new gff file,
-# - that will hopefull work... fingers crossed
-## ONLY MODIFY GFF DATA
-
 import csv, sys, re
-from cigar import Cigar
 
-# Both of the input files
-# Index 0 is the `.sam` file from bwa
-# Index 1 is the `.gff` file from prokka
+# The fist input should be the `.sam file
+# The second input should be the `.gff` file
 inFile = [sys.argv[1], sys.argv[2]]
 samData = []
 gffData = []
+supAlign = []
 header = 0
 
 # Might not need some of these either. Will edit after I figure out how to
@@ -24,7 +18,10 @@ samStart = []
 samSeq = []
 samCigar = []
 
-supAlign = []
+# Function to reverse a string
+def reverse(string):
+    string = string[::-1]
+    return string
 
 # Storing the `.sam` file data into a list
 with open(inFile[0], 'r') as sam:
@@ -84,6 +81,8 @@ for i in range(len(samNodes)):
     if samFlags[i] == '2048':
         supAlign.append(i)
 
+# will probably just modify lists in place once everything is working
+# this way there won't be so many lists taking up memory :)
 node = []
 start = []
 seq = []
@@ -100,6 +99,8 @@ for i in range(len(samNodes)):
 
 # Now we need to adjust the sequences that remain to match their cigar string so
 # - that everything will match nicely with the alignment
+# Nodes that have the 16 flag (0x10) are mapped in reverse and must therefore
+# - be flipped before they are modified, again, so they match the alignment
 
 # Case1 ( M/X/=):
 # - start at the specified mapping position, set counter to 1
@@ -119,10 +120,19 @@ for i in range(len(samNodes)):
 ## P ADDS SPACE TO READ ? (DON'T KNOW YET, NO READS HAVE HAD PADDING!)
 ## S/H CLIP FROM READ (SHORTEN IT)
 ## I REMOVES BASES FROM READ (looks that way in tablet and artemis)
+
+mod_seq = []
+mod_seq_test = []
+
 for i in range(len(seq)):
     smoke = re.findall(r'([\d]+)([A-Z])', cigar[i])
     base_num = 1
-    temp_seq = list(seq[i])
+    if flag[i] == '16':
+        temp_seq = list(reverse(seq[i]))
+        print('-')
+    else:
+        temp_seq = list(seq[i])
+        print('+')
     print(node[i])
     print("start len seq: " + str(len(temp_seq)))
     print("start base num: " + str(base_num))
@@ -134,34 +144,98 @@ for i in range(len(seq)):
             while count != int(op[0]):
                 base_num += 1
                 count += 1
-            print("iter")
-            print("current base num: " + str(base_num))
-        elif op[1] == 'N' or op[1] == 'D':
+            #print("iter")
+            #print("current base num: " + str(base_num))
+        elif op[1] == 'D':
             # add an N to the read to inflate its length
             while count != int(op[0]):
-                temp_seq.insert(base_num-1, 'N')
+                temp_seq.insert(base_num-1, '*')
                 base_num += 1
                 count += 1
-            print("add")
-            print("current base num: " + str(base_num))
+            #print("add deletion")
+            #print("current base num: " + str(base_num))
+        elif op[1] == 'N':
+            while count != int(op[0]):
+                temp_seq.insert(base_num-1, '-')
+                base_num += 1
+                count += 1
         elif op[1] == 'S' or op[1] == 'H' or op[1] == 'I':
             # Remove the bases from a range
             while count != int(op[0]):
                 del temp_seq[base_num-1]
                 count +=1
-            print("remove")
-            print("current base num: " + str(base_num))
+            #print("remove")
+            #print("current base num: " + str(base_num))
         else:
             # I don't yet know how padding affects the reads for this...
             print("P")
+    mod_seq.append(str(temp_seq))
     print("end len seq: " + str(len(temp_seq)))
     print("end base num: " + str(base_num))
     print(" ")
 
 print(cigar[23])
 
-    #print(len(temp_seq))
-    #print(count)
+##### TEST LOOP WITH SEQUENCE INFLATION
+
+for i in range(len(seq)):
+    smoke = re.findall(r'([\d]+)([A-Z])', cigar[i])
+    base_num = 1
+    if flag[i] == '16':
+        temp_seq = list(reverse(seq[i]))
+        print('-')
+    else:
+        temp_seq = list(seq[i])
+        print('+')
+    print(node[i] + " INFLATION TEST")
+    print("start len seq: " + str(len(temp_seq)))
+    print("start base num: " + str(base_num))
+    for j in range(len(smoke)):
+        count = 0
+        op = smoke[j]
+        if op[1] == 'M' or op[1] == 'X' or op[1] == '=':
+            # iterate through the sequence without modifying the base
+            while count != int(op[0]):
+                base_num += 1
+                count += 1
+            #print("iter")
+            #print("current base num: " + str(base_num))
+        elif op[1] == 'D':
+            # add an N to the read to inflate its length
+            while count != int(op[0]):
+                temp_seq.insert(base_num-1, '*')
+                base_num += 1
+                count += 1
+            #print("add deletion")
+            #print("current base num: " + str(base_num))
+        elif op[1] == 'N':
+            while count != int(op[0]):
+                temp_seq.insert(base_num-1, '-')
+                base_num += 1
+                count += 1
+        elif op[1] == 'S' or op[1] == 'H' or op[1] == 'I':
+            # Remove the bases from a range
+            while count != int(op[0]):
+                temp_seq.insert(base_num-1, '$')
+                base_num += 1
+                count += 1
+            #print("remove")
+            #print("current base num: " + str(base_num))
+        else:
+            # I don't yet know how padding affects the reads for this...
+            print("P")
+    mod_seq_test.append(str(temp_seq))
+    print("end len seq: " + str(len(temp_seq)))
+    print("end base num: " + str(base_num))
+    print(" ")
+
+print(cigar[23])
+print(len(mod_seq[23]))
+print(mod_seq[23])
+print(" ")
+print(cigar[23])
+print(len(mod_seq_test[23]))
+print(mod_seq_test[23])
 
 # Need position 4 [index 3] (feature start), position 5 [index 4] (feature end)
 for i in range(len(gffData)):
