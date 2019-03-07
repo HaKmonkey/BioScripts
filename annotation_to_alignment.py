a7f888b60be6fb95e9ceef2deef567afc982b526
hakmonkey <hakmonkey@gmail.com>
2019-03-07 02:19:22 -0700

    New SAM File
    Creating a new `.sam` file with the new node names works! Now to
    create the new `.gff` file and test if the features will map to the
    alignment.

--- annotation_to_alignment.py
+++ annotation_to_alignment.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
+##################
+# Import & Input #
+##################
+
 import csv, sys, re
+
+## INCLUDE A WARNING HERE ##
+## Check that 2 files were Input
+## Check that file 1 is `.sam`
+## Check that file 2 is `.gff`
+
+# A list of the input files
+# The fist input should be the `.sam file
+# The second input should be the `.gff` file
+inFile = [sys.argv[1], sys.argv[2]]
 
 #############
 # Functions #
@@ -11,63 +25,46 @@ def reverse(string):
     string = string[::-1]
     return string
 
-#########################
-# Variable Declarations #
-#########################
+####################
+# Parsing Inputs 1 #
+####################
 
-# A list of the input files
-# The fist input should be the `.sam file
-# The second input should be the `.gff` file
-inFile = [sys.argv[1], sys.argv[2]]
-
-# This is the `.sam` file information
+# This is the `.sam` file
 samData = []
-# Might not need some of these either. Will edit after I figure out how to
-# - propperly edit the gff information
-samNodes = []
-samFlags = []
-samStarts = []
-samSeqs = []
-samCigars = []
-
-# This is the `.gff` file information
-gffData = []
-gffNodes = []
-gffStarts = []
-gffEnds = []
-
-# Variable for filtering the `.gff` information
-header = 0
-
-# Variable for grouping the gene features by NODE_ID
-groups = []
-info_groups = []
-info = []
-
-# Variable to contain the modified sequences
-mod_seq = []
-
-##################
-# Parsing Inputs #
-##################
+originalSam = []
 
 # Storing the `.sam` file data into a list
 with open(inFile[0], 'r') as sam:
     samreader = csv.reader(sam, delimiter = '\t')
+    for row in samreader:
+        originalSam.append(row)
+sam.close()
+
+with open(inFile[0], 'r') as sam:
+    samreader = csv.reader(sam, delimiter = '\t')
     # Skip the first 2 lines in the sam file
-    [next(samreader) for _ in range(2)]
+    [next(samreader) for i in range(2)]
     for row in samreader:
         samData.append(row)
+sam.close()
+
+####################
+# Parsing Inputs 2 #
+####################
+
+# This is the `.gff` file
+gffData = []
 
 # Storing the `.gff` file data into a list
 with open(inFile[1], 'r') as gff:
     gffreader = csv.reader(gff, delimiter = '\t')
     for row in gffreader:
         gffData.append(row)
+gff.close()
 
-##################
-# Cleaning Files #
-##################
+#####################
+# Cleaning GFF File #
+#####################
 
 # Finding where the sequence information starts and remove it because we don't
 # - need it for the changes we want to make
@@ -77,6 +74,9 @@ for i in range(len(gffData)):
         break
 del gffData[fasta:len(gffData)]
 
+# Variable for filtering the `.gff` information
+header = 0
+
 # Finding and removing the header to the `.gff` file because we don't need it
 for i in range(len(gffData)):
     if re.search(r"\#", str(gffData[i])):
@@ -84,9 +84,15 @@ for i in range(len(gffData)):
 head = gffData[0:header] # just in case I need this for making the gff file
 del gffData[0:header]
 
-#################
-# Grabbing Data #
-#################
+#####################
+# Grabbing SAM Data #
+#####################
+
+# This is the `.sam` file information
+samNodes = []
+samFlags = []
+samSeqs = []
+samCigars = []
 
 # Need [index 0] Node name, [index 3] left most base,[index 5] the cigar string
 # - , and [index 9] sequence
@@ -97,17 +103,23 @@ for i in range(len(samData)):
     node = node[2:len(node)-2]
     flag = temp[1]
     flag = flag[1:len(flag)-2]
-    start = temp[3]
-    start = start[1:len(start)-2]
     seq = temp[9]
     seq = seq[1:len(seq)-2]
     cigar = temp[5]
     cigar = cigar[1:len(cigar)-2]
     samNodes.append(node)
     samFlags.append(flag)
-    samStarts.append(start)
     samSeqs.append(seq)
     samCigars.append(cigar)
+
+#####################
+# Grabbing GFF Data #
+#####################
+
+# This is the `.gff` file information
+gffNodes = []
+gffStarts = []
+gffEnds = []
 
 # Need [index 0] Node name, [index 3] start of the feature, and [index 9] end
 # - of the feature
@@ -124,18 +136,12 @@ for i in range(len(gffData)):
     gffStarts.append(start)
     gffEnds.append(end)
 
-##################################
-# Modifying Sequences & Features #
-##################################
+#######################
+# Modifying Sequences #
+#######################
 
-# Sorting feature indices by which node they match to
-for i in range(len(samNodes)):
-    temp = []
-    print(samNodes[i])
-    for j in range(len(gffNodes)):
-        if gffNodes[j] == samNodes[i]:
-            temp.append(j)
-    groups.append(temp)
+# Variable to contain the modified sequences
+mod_seq = []
 
 # Now we need to adjust the sequences that remain to match their cigar string so
 # - that everything will match nicely with the alignment
@@ -183,6 +189,24 @@ for i in range(len(samSeqs)):
     else:
         mod_seq.append(''.join(temp_seq))
 
+######################
+# Modifying Features #
+######################
+
+# Variable for grouping the gene features by NODE_ID
+groups = []
+
+# Sorting feature indices by which node they match to
+for i in range(len(samNodes)):
+    temp = []
+    for j in range(len(gffNodes)):
+        if gffNodes[j] == samNodes[i]:
+            temp.append(j)
+    groups.append(temp)
+
+# Variable for storing new position info for all features
+info = []
+
 # Adjusting annotations
 for i in range(len(groups)):
     # need to parse through samCigar[i]
@@ -227,3 +251,76 @@ for i in range(len(groups)):
             temp.append(start)
             temp.append(end)
         info.append(temp)
+
+########################
+# Adjusting Node Names #
+########################
+
+# Variable for storing original node number
+id = []
+
+for i in range(len(samNodes)):
+    temp = re.findall(r'NODE_(\d+)_', samNodes[i])
+    temp = str(temp)
+    id.append(temp)
+
+count = 1
+bool = False
+
+for i in range(len(samNodes)):
+    if bool == True and id[i] == id[i-1]:
+        count += 1
+        samNodes[i] = ('%s%s%s%d') %("NODE_", str(id[i])[2:len(id[i])-2], "_p",
+            count)
+    else:
+        count = 1
+        samNodes[i] = ('%s%s%s%d') %("NODE_", str(id[i])[2:len(id[i])-2], "_p",
+            count)
+        bool = True
+
+###############################
+# Modify Original SAM Headers #
+###############################
+
+samString = ""
+temp = originalSam[0]
+
+for i in range(len(temp)):
+    samString += temp[i]
+    samString += "\t"
+originalSam[0] = samString
+
+samString = ""
+temp = originalSam[1]
+
+for i in range(len(temp)):
+    samString += temp[i]
+    samString += "\t"
+originalSam[1] = samString
+
+#############################
+# Modify Original SAM Nodes #
+#############################
+
+for i in range(2,len(originalSam)):
+    temp = str(originalSam[i])
+    temp = temp.split(' ')
+    temp[0] = samNodes[i-2]
+    for j in range(1,len(temp)):
+        spot = temp [j]
+        spot = spot[1:len(spot)-2]
+        temp[j] = spot
+    temp = '\t'.join(temp)
+    originalSam[i] = temp
+
+######################
+# Write New SAM File #
+######################
+
+file = open("a2a.sam","w")
+
+for i in range(len(originalSam)):
+    file.write(originalSam[i])
+    file.write("\n")
+
+file.close()
