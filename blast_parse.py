8e537df2bf31a4289f443a676a7e118868a87c39
hakmonkey <hakmonkey@gmail.com>
2019-03-10 18:05:49 -0600

    Created Working Test Case
    Created a working test case that outputs a single table.
    
    Need to now combine different parts to create multiple tables. One for
    each query sequence that has at least one hit.

--- blast_parse.py
+++ blast_parse.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 
+##################
+# Import & Input #
+##################
+
 import re
 import sys
 import csv
+import numpy as np
 from astropy.table import Table, Column
 
 # blastn output file
 inFile = sys.argv[1]
+
+#############
+# Functions #
+#############
+
+# Function that splits the match percentages into different groups
+def splitz(seq, sep):
+  group = []
+  for num in seq:
+    if num != sep:
+      group.append(num)
+    elif group:
+      yield group
+      group = []
+
+#################
+# Parsing Blast #
+#################
 
 blast = []
 
@@ -28,24 +51,137 @@ with open(inFile, 'r') as b:
 
 b.close()
 
-# START
-# Query= NODE_1_length_937914_cov_4.753038 
-# END
-# Effective search space used: 2035474639056
+########################
+# Finding Query Chunks #
+########################
 
-start = []
-end = []
+start_chunk = []
+end_chunk = []
 
 for i in range(len(blast)):
   if re.match(r'\[\'Query=', str(blast[i])):
-    start.append(i)
+    start_chunk.append(i)
   if re.match(r'\[\'Effective', str(blast[i])):
-    end.append(i)
-      
-print(start)
-print(len(start))
-print(end)
-print(len(end))
+    end_chunk.append(i)
+
+########################
+# Parsing Query Chunks #
+########################
+
+'''
+query = ""
+start_sig = None
+end_sig = None
+
+for i in range(len(start_chunk)):
+  for j in range(start_chunk[i], end_chunk[i] + 1):
+    if re.match(r'Query= ', blast[j]):
+      query = str(blast[j])
+    if re.match(r'\[\'Sequences producing significant alignments', blast[j]):
+      start_sig = j
+    if re.match(r'\[\'\> ', blast[j]):
+      end_sig = j
+'''
+
+##############################################################
+# Getting the Query and Significant Alignment Sequences TEST #
+##############################################################
+
+query = ""
+start_sig = None
+end_sig = None
+
+for j in range(start_chunk[0], end_chunk[0]+1):
+  if re.match(r'\[\'Query= ', str(blast[j])):
+      query = str(blast[j])
+  if re.match(r'\[\'Sequences producing significant alignments', str(blast[j])):
+    start_sig = j
+  if re.match(r'\[\'\> ', str(blast[j])):
+    end_sig = j
+    break
+
+##################################
+# Finding Hits Per Sequence TEST #
+##################################
+
+seq = [] # For sequences column
+hits = []
+x = False
+
+for k in range(start_chunk[0], end_chunk[0]+1):
+  if re.match(r'\[\'\> ', str(blast[k])):
+    seq.append(str(blast[k]))
+    x = True
+    continue
+  while x:
+    if re.match(r'\[\'\> ', str(blast[k])) or k == end_chunk[0]:
+      x = False
+      hits.append(" ")
+      k += 1
+    else:
+      hits.append(str(blast[k]))
+      k += 1
+
+##############################
+# Pulling Hits and Gaps TEST #
+##############################
+
+match = [] 
+gaps = []
+length = []
+
+for i in range(len(hits)):
+  temp_m = str(re.findall(r'Identities = [0-9]+/[0-9]+ \(([0-9]+)\%\)', hits[i]))
+  temp_m = temp_m[2:len(temp_m)-2]
+  match.append(temp_m)
+  temp_g = str(re.findall(r'Gaps = [0-9]+/[0-9]+ \(([0-9]+)\%\)', hits[i]))
+  temp_g = temp_g[2:len(temp_g)-2]
+  gaps.append(temp_g)
+  temp_l = str(re.findall(r'Identities = [0-9]+/([0-9]+) \([0-9]+\%\)', hits[i]))
+  temp_l = temp_l[2:len(temp_l)-2]
+  length.append(temp_l)
+
+
+#################
+# Grouping TEST #
+#################
+
+# match and gap % grouped by which sequence they hit
+matchp = list(splitz(match, '')) # for avg match % column
+gapp = list(splitz(gaps, '')) # for avg gap % column
+length = list(splitz(length, '')) # for avg hit length column
+num_hits = [] # for the hits column
+
+# gets the mean for each group
+for i in range(len(matchp)):
+  matchp[i] = [int(x) for x in matchp[i]]
+  num_hits.append(len(matchp[i]))
+  matchp[i] = np.mean(matchp[i])
+  gapp[i] = [int(x) for x in gapp[i]]
+  gapp[i] = np.mean(gapp[i])
+  length[i] = [int(x) for x in length[i]]
+  length[i] = np.mean(length[i])
+
+#########################
+# Printing a Table TEST #
+#########################
+
+t = Table()
+#t['Sequence'] = seq
+t['Hits'] = num_hits
+t['Avg. Hit Length'] = length
+t['Avg. Match %'] = matchp
+t['Avg. Gaps %'] = gapp
+#t['Best']
+
+print(seq)
+print(num_hits)
+print(length)
+print(matchp)
+print(gapp)
+print(" ")
+
+print(t)
 
 # Table
 ## Query
@@ -56,13 +192,15 @@ print(len(end))
 ## Average Length of hits
 
 
-# | Query Sequence | Database Sequence | Hits | Avg Hit Length | Avg Match % | Avg Gap % | Best |
-# | -------------- | ----------------- | ---- | -------------- | ----------- | --------- | ---- |
-# | NODE_1         | Pseudomonas       | 135  | 40000          | 95%         | 0.5%      | X    |
-# | NODE_1         | Annandia          | 5    | 60             | 90%         | 1%        |      |
+# NODE_1
+#
+# | Sequence    | Hits | Avg Hit Length | Avg Match % | Avg Gap % | Best |
+# | ----------- | ---- | -------------- | ----------- | --------- | ---- |
+# | Pseudomonas | 135  | 40000          | 95%         | 0.5%      | X    |
+# | Annandia    | 5    | 60             | 90%         | 1%        |      |
 
 
-
+'''
 file = open("parsed_blast.txt","w")
 
 # writing everything but the footer
@@ -73,3 +211,4 @@ for i in range(len(blast)-6):
   file.write("\n")
 
 file.close()
+'''
